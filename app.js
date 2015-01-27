49f8818407c899f83b3ad6d6d4cd9e664052caee
Edward Raison <edwardraison@Edwards-MacBook-Pro.local>
2015-01-27 07:24:24 -0500

    db keepalive

--- app.js
+++ app.js
@@ -17,22 +17,38 @@ app.configure(function() {
 });
 var traverse = require('traverse');
 var mysql = require('mysql');
-var connection = mysql.createConnection({
+var db_config = {
     database: 'heroku_679c9f4e343c163',
     host: 'us-cdbr-iron-east-01.cleardb.net',
     user: 'bdbc3e7d415a19',
     password: 'aacfb504',
-});
+};
 
-connection.connect(function(err) {
-    if (err) {
-        console.error('error connecting: ' + err.stack);
-        return;
+
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
     }
+  });
+}
 
-    console.log('connected as id ' + connection.threadId);
-});
-
+handleDisconnect();
 
 
 app.configure('development', function() {
@@ -129,7 +145,6 @@ app.get('/api/tree/:startNode', function(req, res) {
 
 
         var theTree = convert(rows);
-        console.log("the start node " + req.params.startNode);
         if (req.params.startNode !== "0") {
 
             function scan(obj) {
